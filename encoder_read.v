a95f1ec8f6e2de2438f0f5b4314842611219d95e
keisuke0821 <kataoka.keisuke.45x@st.kyoto-u.ac.jp>
2023-12-20 14:50:55 +0900

    1220

--- encoder_read.v
+++ encoder_read.v
@@ -13,12 +13,12 @@ module encoder_read(
     assign in_num2[1:0] = {in_reg2[1], (in_reg2[1]^in_reg2[0])};
 
     always @(posedge clk) begin
-        in_reg1[1:0] <= in[1:0];
-        in_reg2[1:0] <= in_reg1[1:0];
+        in_reg1[1:0] <= in[1:0];    //クロックが入るとin_reg1は入力信号をそのまま受け取る
+        in_reg2[1:0] <= in_reg1[1:0];  //in_reg2はin_reg1の値を受けとる
     end
 
     always @(posedge clk) begin
-        case (in_num1[1:0] - in_num2[1:0])
+        case (in_num1[1:0] - in_num2[1:0]) //差をとってそれに応じてoutを生成する
             2'd1: out[1:0] <= 2'b01;
             2'd3: out[1:0] <= 2'b10;
             default: out[1:0] <= 2'b00;
